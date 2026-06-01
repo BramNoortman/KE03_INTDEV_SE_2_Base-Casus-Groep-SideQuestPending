390703d875a4249e18187a6b388d481b92d86e2d
RoyTeheuxZuyd <2506589teheux@zuyd.nl>
2026-06-01 20:24:31 +0200

    Add Products controller, view and styles
    Introduce product listing UI and controller: add ProductsController (uses IProductRepository and orders products by Id), a Razor view (Views/Products/Index.cshtml) bound to IEnumerable<Product>, and a minimal PageModel. Add CSS rules for the products page and toolbar in site.css. Update project file to include wwwroot Media folders and bump solution Visual Studio version to 18.

---
+++ KE03_INTDEV_SE_2_Base/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+    public IActionResult Index()
+        {
+            var products = _productRepository.GetAllProducts().OrderBy(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
