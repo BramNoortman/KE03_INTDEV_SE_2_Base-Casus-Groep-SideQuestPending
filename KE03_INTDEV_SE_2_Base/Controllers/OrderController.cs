89a378aedea45cf0a062251cdb5afcf6227c649e
RoyTeheuxZuyd <2506589teheux@zuyd.nl>
2026-06-02 10:28:42 +0200

    Order page W.I.P.
    Work in progress voor de Order overview page

---
+++ KE03_INTDEV_SE_2_Base/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class OrderController: Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+     public IActionResult Index(string search)
+        {
+            var orders = _orderRepository.GetAllOrders();
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                orders = orders.Where(o => o.Customer.Name.ToLower().Contains(search.ToLower()));
+            }
+
+            var result = orders.OrderBy(p => p.Id).ToList();
+            ViewBag.Search = search;
+            return View(result);
+        }
+    }
+}
