72731e0be34eca0fe931b5f44b634043b8d1a8a1
Lars Voncken <lars.voncken2006@gmail.com>
2026-06-04 12:51:08 +0200

    Order implementation start

--- DataAccessLayer/Models/Order.cs
+++ DataAccessLayer/Models/Order.cs
@@ -9,12 +9,17 @@ namespace DataAccessLayer.Models
 {
     public class Order
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public DateTime OrderDate { get; set; }
 
+        [Required]
         public int CustomerId { get; set; }
         
+        [Required]
         public Customer Customer { get; set; } = null!;
 
         public ICollection<Product> Products { get; } = new List<Product>();
--- KE03_INTDEV_SE_2_Base/Controllers/OrderController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/OrderController.cs
@@ -1,30 +1,167 @@
-﻿using DataAccessLayer.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer;
+using DataAccessLayer.Models;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
-    public class OrderController: Controller
+    public class OrderController : Controller
     {
-        private readonly IOrderRepository _orderRepository;
+        private readonly MatrixIncDbContext _context;
 
-        public OrderController(IOrderRepository orderRepository)
+        public OrderController(MatrixIncDbContext context)
         {
-            _orderRepository = orderRepository;
+            _context = context;
         }
 
-     public IActionResult Index(string search)
+        // GET: Order
+        public async Task<IActionResult> Index(string search)
         {
-            var orders = _orderRepository.GetAllOrders();
+            var query = _context.Orders.AsQueryable();
+
             if (!string.IsNullOrWhiteSpace(search))
             {
-                orders = orders.Where(o => o.Customer.Name.ToLower().Contains(search.ToLower()));
+                query = query.Where(o => o.Name.Contains(search) || o.Address.Contains(search) || o.Active.ToString().Contains(search));
             }
 
-            var result = orders.OrderBy(p => p.Id).ToList();
             ViewBag.Search = search;
-            return View(result);
+            var customers = await query.OrderBy(c => o.Id).ToListAsync();
+
+            return View(customers);
+        }
+
+        // GET: Order/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Order
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Order/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Active")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Order/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Order.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Order/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Active")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Order/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Order
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Order.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Order.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Order.Any(e => e.Id == id);
         }
     }
 }
