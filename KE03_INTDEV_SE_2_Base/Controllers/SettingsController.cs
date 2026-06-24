a1c12916d3c94d31759e1d2a540bf21b9d38c1cf
Lars Voncken <lars.voncken2006@gmail.com>
2026-06-24 13:26:46 +0200

    Final Big Commit
    Under Construction Page (WorkInProgress) toegevoegd en aanpassingen in finance om errors te verkomen.

---
+++ KE03_INTDEV_SE_2_Base/Controllers/SettingsController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class WorkInProgressController : Controller
+    {
+        private readonly MatrixIncDbContext _context;
+
+        public WorkInProgressController(MatrixIncDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        // searchbar & sorting
+        public async Task<IActionResult> Index(string search, string sortOrder)
+        {
+            var query = _context.Customers.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                search = search.Trim().ToLower();
+                query = query.Where(c => c.Id.ToString().Contains(search) || (c.Name ?? "").ToLower().Contains(search) || (c.Address ?? "").ToLower().Contains(search) || c.Active.ToString().Contains(search));
+            }
+
+            ViewBag.Search = search;
+            ViewBag.CurrentSort = sortOrder; //for arrow direction
+            ViewBag.IdSort = sortOrder == "id_desc" ? "" : "id_desc";
+            ViewBag.NameSort = sortOrder == "name" ? "name_desc" : "name";
+            ViewBag.AddressSort = sortOrder == "address" ? "address_desc" : "address";
+            ViewBag.ActiveSort = sortOrder == "active" ? "active_desc" : "active";
+
+            query = sortOrder switch
+            {
+                "id_desc" => query.OrderByDescending(c => c.Id),
+                "name" => query.OrderBy(c => c.Name),
+                "name_desc" => query.OrderByDescending(c => c.Name),
+                "address" => query.OrderBy(c => c.Address),
+                "address_desc" => query.OrderByDescending(c => c.Address),
+                "active" => query.OrderBy(c => c.Active),
+                "active_desc" => query.OrderByDescending(c => c.Active),
+                _ => query.OrderBy(c => c.Id)
+            };
+
+            return View(await query.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Active")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Active")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
